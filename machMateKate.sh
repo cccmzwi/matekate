dcbb4865966731775fbffdafc6272fdc2e715f1c
Tanjeff-N. Moos <tanjeff@cccmz.de>
2009-03-22 14:48:16 +0000

    machMateKate hinzugefügt
    git-svn-id: https://svn.cccmz.de/matekate@2 223dcf81-e940-4002-9f84-9aebb59a28bd

---
+++ machMateKate.sh
@@ -0,0 +1,20 @@
+matefile="matekate.txt"
+nodeURL="http://www.informationfreeway.org/api/0.5/node[club-mate=yes]"
+wayURL="http://www.informationfreeway.org/api/0.5/way[club-mate=yes]"
+
+
+rm -f "$matefile"
+
+# get data from server
+while (! wget "$nodeURL" -O matenodes.osm)
+do
+    echo "getting data from server failed (nodes)."
+done
+
+while (! wget "$wayURL" -O mateways.osm)
+do
+    echo "getting data from server failed (ways)."
+done
+
+xsltproc osm2text.xsl matenodes.osm | sed 's/^\s\s//g' > matekate.txt
+xsltproc osm2text.xsl mateways.osm | sed 's/^\s\s//g' | sed '/^lat\t/ d' >> matekate.txt
