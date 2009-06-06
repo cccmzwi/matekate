77b9620d4557582813fa4c9ab7dfba3acbfb483a
Tanjeff-N. Moos <tanjeff@cccmz.de>
2009-06-06 08:17:43 +0000

    Add better error handling. Add more metainfo to matecount.html.
    git-svn-id: https://svn.cccmz.de/matekate@7 223dcf81-e940-4002-9f84-9aebb59a28bd

--- machMateKate.sh
+++ machMateKate.sh
@@ -1,25 +1,41 @@
 matefile="matekate.txt"
-nodeURL="http://www.informationfreeway.org/api/0.6/node[club-mate=yes]"
-wayURL="http://www.informationfreeway.org/api/0.6/way[club-mate=yes]"
+#nodeURL="http://www.informationfreeway.org/api/0.6/node[club-mate=yes]"
+nodeURL="http://xapi.openstreetmap.org/api/0.5/node[club-mate=yes]"
+#wayURL="http://www.informationfreeway.org/api/0.6/way[club-mate=yes]"
+wayURL="http://xapi.openstreetmap.org/api/0.5/way[club-mate=yes]"
 
 
-rm -f "$matefile"
-
-# get data from server
-while (! wget "$nodeURL" -O matenodes.osm)
-do
+# get nodes
+if (! wget "$nodeURL" -t 3 -O matenodes.osm.part)
+then
     echo "getting data from server failed (nodes)."
-done
+    rm -f matenodes.osm.part
+    exit 1
+else
+    mv matenodes.osm.part matenodes.osm
+fi
+xsltproc osm2text.xsl matenodes.osm | sed 's/^\s\s//g' > "$matefile.part"
 
-while (! wget "$wayURL" -O mateways.osm)
-do
+# get ways
+if (! wget "$wayURL" -t 3 -O mateways.osm.part)
+then
     echo "getting data from server failed (ways)."
-done
+    rm -f mateways.osm.part
+    exit 1
+else
+    mv mateways.osm.part mateways.osm
+fi
+xsltproc osm2text.xsl mateways.osm | sed 's/^\s\s//g' | sed '/^lat\t/ d' >> "$matefile.part"
 
-xsltproc osm2text.xsl matenodes.osm | sed 's/^\s\s//g' > matekate.txt
-xsltproc osm2text.xsl mateways.osm | sed 's/^\s\s//g' | sed '/^lat\t/ d' >> matekate.txt
+# prepare matefile
+rm -f "$matefile"
+mv "$matefile.part" "$matefile"
 
-# calculate number of shown Mate POIs
+
+# prepare file with metainfos
 matecount=`cat matekate.txt | egrep '^[[:digit:]]' | wc -l`
 
-echo "<p>Derzeit sind $matecount Mate-Zugangspunkte eingetragen.</p>" > matecount.html
+echo "<p>" > matecount.html
+echo "Derzeit sind $matecount Mate-Zugangspunkte eingetragen." >> matecount.html
+echo "Stand: `date '+%d.%m.%Y, %H:%M'`" >> matecount.html
+echo "</p>" >> matecount.html
